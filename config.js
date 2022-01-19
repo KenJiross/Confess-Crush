a215918dc42c31e25bcc5cdc08c366a5ecf9dccf
KenJiross <98022426+KenJiross@users.noreply.github.com>
2022-01-19 17:46:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    titleWeb: "bé yewww của anh :>",
+    introTitle: 'bé nào đó tên vyy :3',
+    introDesc: `gọi em là tình cuốii
+    làm em cười rồi tínhhh
+    mình làm tình rồi cướiiii
+    yêu thì nói yêu nhee chứ ko có điêuuu`,
+    btnIntro: '^^ahjhj:3i^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bé lại yêu anhh:3♥',
+    btnReply: 'Gửi cho bé <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'í xời anh biết mà 🥰. Yêu bé nhiềuuu 😘😘',
+    messDesc: 'Tối nay 7h bé có muốn call tềnh cảm hemm.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
